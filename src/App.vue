9b9d43e265abc9de6b7a4f9d4fff5822a95cc221
devGBR <gfcontroladoriageral@gmail.com>
2022-09-21 10:07:20 -0300

    Navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -24,5 +24,21 @@ import Footer from './components/Footer.vue';
 
  </script>
 <style>
+ *{
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+ }
+ .main-container{
+  margin: 50px;
+  min-height: 345px;
+ }
 
+ h1{
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: rgb(50, 49, 49);
+ }
 </style>
